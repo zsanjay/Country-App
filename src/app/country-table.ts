3757a4840db98ed71d566e1e02ce7b6a20ed44db
Sanjay Mehta <sanjay.mehta@inventum.net>
2020-10-14 20:28:37 +0530

    country app code

---
+++ src/app/country-table.ts
@@ -0,0 +1,13 @@
+
+export interface CountriesTable {
+    name: string;
+    capital: string;
+    flag: string;
+    region: string;
+    alpha2Code: string;
+    alpha3Code: string;
+    currencies : any[]; 
+    borders : string[];
+    subregion : string;
+    timezones : any;
+}
